881a4263f01e5a8ba04d929ef96ee668af8cbfa5
NicolasNewman <nanewman01@gmail.com>
2026-05-06 17:46:37 +0900

    chore: removed example

--- examples/react/README.md
+++
@@ -1,3 +0,0 @@
-# tauri-plugin-libmpv-example
-
-A example program for embedding mpv player in Tauri via libmpv.
--- examples/react/src-tauri/src/main.rs
+++
@@ -1,5 +0,0 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-fn main() {
-    tauri_plugin_libmpv_example_lib::run();
-}
